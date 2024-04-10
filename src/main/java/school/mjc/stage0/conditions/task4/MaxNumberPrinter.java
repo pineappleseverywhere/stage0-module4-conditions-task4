9160064a1d5e3943565e665925c736d1195ae3ae
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-10 18:53:41 -0400

    done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max12 = (first >= second) ? first : second;
+
+        int max = (max12 >= third) ? max12 : third;
+
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+        int max12 = (first >= second) ? first : second;
+
+        int max = (max12 >= third) ? max12 : third;
+
+        System.out.println(max);
     }
 }
